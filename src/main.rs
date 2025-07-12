0c91f46b178bbb8881dfaacfe5c217205d0d154b
umzi <ab5447612@gmail.com>
2025-07-13 01:48:25 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,208 @@
+mod slashers;
+
+use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
+use pepecore::enums::ImgColor;
+use pepecore::read::read_in_path;
+use pepecore_array::{ SVec, Shape};
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+use std::{fs, io};
+use walkdir::WalkDir;
+use crate::slashers::central_scan::slasher_central;
+use crate::slashers::standard::slasher;
+
+/// CLI-парсер
+#[derive(Parser, Debug)]
+#[command(
+    name = "slasher-cli",
+    about = "Нарезает изображения в папках и выводит прогресс"
+)]
+struct Args {
+    /// Входная папка с изображениями
+    #[arg(short, long)]
+    input: PathBuf,
+
+    /// Папка для результатов (по умолчанию рядом с input)
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+
+    /// Порог потока (thread)
+    #[arg(short = 't', long, default_value_t = 0)]
+    thread: u8,
+
+    /// Высота кропа в пикселях
+    #[arg(short = 'h', long, default_value_t = 15_000)]
+    crop_height: usize,
+
+    /// Отступ ауры в пикселях
+    #[arg(short = 'a', long, default_value_t = 100)]
+    aura_margin: usize,
+
+    /// Шаг сканирования в пикселях
+    #[arg(short = 's', long, default_value_t = 5)]
+    scan_step: usize,
+
+    #[arg(short = 'f', long, default_value_t = false)]
+    folder_mode: bool,
+    
+    #[arg(short = 'c', long, default_value_t = false)]
+    central_scan: bool,
+}
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+    let input_root = args.input.canonicalize()?;
+    let output_root = args.output.unwrap_or_else(|| input_root.clone());
+    fs::create_dir_all(&output_root)?;
+
+    let mut images_grouped: Vec<Vec<PathBuf>> = Vec::new();
+
+    if args.folder_mode {
+        let mut folder_map: HashMap<PathBuf, Vec<PathBuf>> = HashMap::new();
+
+        for entry in WalkDir::new(&input_root).into_iter().filter_map(Result::ok) {
+            let p = entry.path();
+            if p.is_file() {
+                // Получаем путь относительно корня
+                let rel_folder = p
+                    .parent()
+                    .unwrap()
+                    .strip_prefix(&input_root)
+                    .unwrap_or_else(|_| Path::new(""))
+                    .to_path_buf();
+
+                folder_map
+                    .entry(rel_folder)
+                    .or_default()
+                    .push(p.to_path_buf());
+            }
+        }
+
+        images_grouped = folder_map.into_values().collect();
+    } else {
+        for entry in WalkDir::new(&input_root).into_iter().filter_map(Result::ok) {
+            let p = entry.path();
+            if p.is_file() {
+                images_grouped.push(vec![p.to_path_buf()]);
+            }
+        }
+    }
+    let pb = ProgressBar::new(images_grouped.len() as u64);
+    pb.set_style(
+        ProgressStyle::with_template("[{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} {msg}")
+            .unwrap()
+            .progress_chars("##-"),
+    );
+
+    let total_images = images_grouped.iter().map(|g| g.len()).sum::<usize>();
+    let pb = ProgressBar::new(total_images as u64);
+    pb.set_style(
+        ProgressStyle::with_template("[{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} {msg}")
+            .unwrap()
+            .progress_chars("##-"),
+    );
+
+    for group in images_grouped {
+        let mut image = read_in_path(&group[0], ImgColor::RGB)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{e:?}")))?;
+        image.as_u8();
+        let mut n = 1;
+        let file_name = if group.len() > 1 {
+            let (mut h, w, c) = image.shape.get_shape();
+            let data = image
+                .get_mut_vec::<u8>()
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{e:?}")))?;
+            for path_index in 1..group.len() {
+                let img_temp = read_in_path(&group[path_index], ImgColor::RGB)
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{e:?}")))?;
+                let t_h = img_temp.shape.get_height();
+                data.extend(
+                    img_temp
+                        .get_data::<u8>()
+                        .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{e:?}")))?,
+                );
+                h += t_h;
+                n += 1
+            }
+            image.shape = Shape::new(h, w, c);
+
+            group
+                .first()
+                .expect("хуя")
+                .parent()
+                .unwrap()
+                .strip_prefix(&input_root)
+                .unwrap_or_else(|_| Path::new(""))
+                .to_path_buf()
+                .display()
+                .to_string()
+        } else {
+            group
+                .first()
+                .expect("хуя")
+                .file_stem() // вернёт OsStr без расширения
+                .unwrap_or_else(|| std::ffi::OsStr::new("unnamed"))
+                .to_string_lossy()
+                .into()
+        };
+
+        pb.set_message(file_name.clone());
+
+        if let Err(e) = process_image(
+            &file_name,
+            &mut image,
+            &output_root,
+            args.thread,
+            args.crop_height,
+            args.aura_margin,
+            args.scan_step,
+            args.central_scan
+        ) {
+            eprintln!("Ошибка обработки {}: {}", &file_name, e);
+        }
+
+        pb.inc(n);
+    }
+
+    pb.finish_with_message("Готово!");
+    Ok(())
+}
+
+fn process_image(
+    file_name: &String,
+    image: &mut SVec,
+    output_root: &Path,
+    thread: u8,
+    crop_height: usize,
+    aura_margin: usize,
+    scan_step: usize,
+    no_normal: bool,
+) -> io::Result<()> {
+    fs::create_dir_all(&output_root.join(file_name))?;
+    if no_normal{
+        slasher_central(
+            image,
+            &output_root.join(file_name),
+            file_name,
+            thread,
+            crop_height,
+            aura_margin,
+            scan_step,
+        );
+    }
+    else {
+        slasher(
+            image,
+            &output_root.join(file_name),
+            file_name,
+            thread,
+            crop_height,
+            aura_margin,
+            scan_step,
+        );
+    }
+    Ok(())
+}
+
+
---
+++ src/slashers/standard.rs
@@ -0,0 +1,119 @@
+use std::path::Path;
+use pepecore::save::svec_save;
+use pepecore_array::{ImgData, SVec, Shape};
+
+pub fn slasher(
+    img: &mut SVec,
+    out_path: &Path,
+    name: &str,
+    thread: u8,
+    crop_height: usize,
+    aura_margin: usize,
+    scan_step: usize,
+) {
+    let (img_height, img_width, channels_opt) = img.shape.get_shape();
+    let num_channels = channels_opt.unwrap();
+    let pixel_data = img.get_data_mut::<u8>().unwrap();
+
+    let mut next_crop_start = crop_height;
+    let mut split_line = 0;
+
+    let scan_range = (0..img_height - 1).step_by(scan_step);
+    let mut crop_boundaries: Vec<usize> = vec![0];
+
+    for row in scan_range {
+        let row_start_idx = row * img_width;
+        let next_row_start_idx = (row + 1) * img_width;
+        let mut max_diff: u8 = 0;
+
+        if row >= next_crop_start {
+            let mut needs_split = true;
+            let compare_base_idx = split_line * img_width * num_channels;
+            if aura_margin + split_line < img_height {
+                for offset in 1..aura_margin {
+                    let mut local_max = 0;
+
+                    for x in 0..img_width {
+                        let ref_idx = ((split_line + offset) * img_width + x) * num_channels;
+                        for ch in 0..num_channels {
+                            local_max = local_max.max(
+                                (pixel_data[compare_base_idx + ch] as i16
+                                    - pixel_data[ref_idx + ch] as i16)
+                                    .abs() as u8,
+                            );
+                        }
+                    }
+
+                    if local_max > thread {
+                        needs_split = true;
+                        break;
+                    } else {
+                        needs_split = false;
+                    }
+                }
+
+                if needs_split {
+                    let mut offset_correction = 0;
+                    for offset in 1..(aura_margin * 2) {
+                        let mut local_max = 0;
+
+                        for x in 0..img_width {
+                            let ref_idx = ((split_line - offset) * img_width + x) * num_channels;
+                            for ch in 0..num_channels {
+                                local_max = local_max.max(
+                                    (pixel_data[compare_base_idx + ch] as i16
+                                        - pixel_data[ref_idx + ch] as i16)
+                                        .abs() as u8,
+                                );
+                            }
+                        }
+
+                        offset_correction = offset / 2;
+                        if local_max > thread {
+                            break;
+                        }
+                    }
+
+                    split_line -= offset_correction;
+                }
+            }
+
+            crop_boundaries.push(split_line);
+            next_crop_start = split_line + crop_height;
+        }
+
+        for x in 0..img_width {
+            let base_current = (row_start_idx + x) * num_channels;
+            let base_next = (next_row_start_idx + x) * num_channels;
+
+            for ch in 0..num_channels {
+                max_diff = max_diff.max(
+                    (pixel_data[base_current + ch] as i16 - pixel_data[base_next + ch] as i16).abs()
+                        as u8,
+                );
+            }
+        }
+
+        if max_diff <= thread {
+            split_line = row;
+        }
+    }
+
+    crop_boundaries.push(img_height);
+
+    for i in 0..crop_boundaries.len() - 1 {
+        let start_y = crop_boundaries[i];
+        let end_y = crop_boundaries[i + 1];
+        let height = end_y - start_y;
+
+        let cropped_slice =
+            &pixel_data[(start_y * img_width * num_channels)..(end_y * img_width * num_channels)];
+        let shape = Shape::new(height, img_width, Some(num_channels));
+        let img_data = ImgData::U8(cropped_slice.to_owned());
+
+        let _ = svec_save(
+            SVec::new(shape, img_data),
+            &out_path.join(format!("{name}_{i}.png")),
+        );
+    }
+}
